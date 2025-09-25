2ae4f5fe2b7dc29b2df79e3b386c14bc634a4a70
Marius Garénaux <marius.garenaux-gruau@irisa.fr>
2025-09-25 17:14:57 +0200

    add: missing links with WorkflowOutputParameter, cli command for visualization

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from cwl2nx import cwl_to_str, CWLToNetworkxConnector
 dir = "workflow_example.cwl.yaml"
 print(cwl_to_str(dir))
 
-dag = CWLToNetworkxConnector(dir).convert_to_networkx()
+# dag = CWLToNetworkxConnector(dir).convert_to_networkx()
 
-for each_node in dag.nodes:
-    print(dag.nodes[each_node])
+# for each_node in dag.nodes:
+#     print(dag.nodes[each_node])
